71f4ece1f910668bfd23fd3b3009db4e614b1482
mbnzim <mbnzim@gmail.com>
2024-06-28 07:03:57 +0200

    updated ball speed

--- js/play.js
+++ js/play.js
@@ -14,35 +14,34 @@ export class Play {
     this.fraction = fraction;
     this.pegs = pegs;
     this.slots = slots;
-    this.pinkBall = PIXI.Sprite.from(`./images/green_ball.png`);
+    this.greenBall = PIXI.Sprite.from(`./images/green_ball.png`);
     this.cost_scored = 0;
     this.bet = bet;
     this.time = Date().split(' ')[4];
     this.slotCost = 0;
     this.top_bounce = top_bounce;
     this.side_bounce = side_bounce;
-    let points = 100;
   }
 
   start() {
     window.document
       .getElementById('points-bet-wrapper__won-flash')
       .classList.remove('points-bet-wrapper__won-flash__animate');
-    this.pinkBall.x = this.openning.x + 5 * this.fraction;
-    this.pinkBall.y = this.openning.y;
-    this.pinkBall.width = 50 * this.fraction;
-    this.pinkBall.height = 50 * this.fraction;
-    this.pinkBall.vy = 0;
-    this.pinkBall.vx = 0;
-    this.app.stage.addChild(this.pinkBall);
+    this.greenBall.x = this.openning.x + 5 * this.fraction;
+    this.greenBall.y = this.openning.y;
+    this.greenBall.width = 50 * this.fraction;
+    this.greenBall.height = 50 * this.fraction;
+    this.greenBall.vy = 0;
+    this.greenBall.vx = 0;
+    this.app.stage.addChild(this.greenBall);
 
     let last_peg = undefined;
     let randomTurn = Math.floor(Math.random() * 2);
 
     let obj = this;
     this.app.ticker.add(function () {
-      obj.pinkBall.y += obj.pinkBall.vy;
-      obj.pinkBall.vy += 0.8;
+      obj.greenBall.y += obj.greenBall.vy;
+      obj.greenBall.vy += 0.7;
 
       for (let pegIndx = 0; pegIndx < obj.pegs.length; pegIndx++) {
         if (
@@ -50,9 +49,9 @@ export class Play {
             obj.pegs[pegIndx].x - 1 * obj.fraction,
             obj.pegs[pegIndx].y,
             obj.pegs[pegIndx].radius,
-            obj.pinkBall.x,
-            obj.pinkBall.y,
-            obj.pinkBall.width / 2
+            obj.greenBall.x,
+            obj.greenBall.y,
+            obj.greenBall.width / 2
           )
         ) {
           obj.pegs[pegIndx].pegBall.tint = 0x23f61e;
@@ -68,10 +67,10 @@ export class Play {
 
           let current_peg = obj.pegs[pegIndx];
 
-          obj.pinkBall.vy *= -obj.top_bounce;
-          obj.pinkBall.y += obj.pinkBall.vy;
-          obj.pinkBall.vx += obj.side_bounce;
-          obj.pinkBall.vx = obj.pinkBall.vx * obj.fraction;
+          obj.greenBall.vy *= -obj.top_bounce + 0.2;
+          obj.greenBall.y += obj.greenBall.vy;
+          obj.greenBall.vx += obj.side_bounce;
+          obj.greenBall.vx = obj.greenBall.vx * obj.fraction;
 
           if (current_peg != last_peg) {
             randomTurn = Math.floor(Math.random() * 2);
@@ -79,9 +78,9 @@ export class Play {
           }
 
           if (randomTurn === 0) {
-            obj.pinkBall.x -= obj.pinkBall.vx;
+            obj.greenBall.x -= obj.greenBall.vx;
           } else if (randomTurn === 1) {
-            obj.pinkBall.x += obj.pinkBall.vx;
+            obj.greenBall.x += obj.greenBall.vx;
           }
 
           break;
@@ -94,12 +93,12 @@ export class Play {
             obj.slots[slotIndx].x,
             obj.slots[slotIndx].y + 40,
             obj.slots[slotIndx].width / 2,
-            obj.pinkBall.x,
-            obj.pinkBall.y,
-            obj.pinkBall.width / 2
+            obj.greenBall.x,
+            obj.greenBall.y,
+            obj.greenBall.width / 2
           )
         ) {
-          obj.app.stage.removeChild(obj.pinkBall);
+          obj.app.stage.removeChild(obj.greenBall);
           let scoredSoundEffect = new Audio('./Sound Effects/scoreEffect.wav');
           scoredSoundEffect.volume = 0.2;
           scoredSoundEffect.play();
@@ -157,10 +156,23 @@ export class Play {
     });
   }
 
+  // isCollision(peg_x, peg_y, peg_r, ball_x, ball_y, ball_r) {
+  //   let circleDistance =
+  //     (peg_x - ball_x) * (peg_x - ball_x) + (peg_y - ball_y) * (peg_y - ball_y);
+  //   return circleDistance <= (peg_r + ball_r) * (peg_r + ball_r);
+  // }
+
   isCollision(peg_x, peg_y, peg_r, ball_x, ball_y, ball_r) {
-    let circleDistance =
-      (peg_x - ball_x) * (peg_x - ball_x) + (peg_y - ball_y) * (peg_y - ball_y);
-    return circleDistance <= (peg_r + ball_r) * (peg_r + ball_r);
+    // Calculate the squared distance between the centers of the peg and the ball.
+    const dx = peg_x - ball_x;
+    const dy = peg_y - ball_y;
+    const circleDistanceSquared = dx * dx + dy * dy;
+
+    // Calculate the squared sum of the radii.
+    const radiiSumSquared = (peg_r + ball_r) ** 2;
+
+    // Check if the squared distance is less than or equal to the squared sum of the radii.
+    return circleDistanceSquared <= radiiSumSquared;
   }
 
   getCostScored(bet, slot_cost) {
